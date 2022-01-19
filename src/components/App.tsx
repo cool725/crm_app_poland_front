6b57e5fd383fa21823688c073b4a52e216981207
cool725 <109250241+cool725@users.noreply.github.com>
2022-01-19 09:34:01 +0200

    fixed not-found issue

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -46,7 +46,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="login" element={<Login />} />
         <Route path="reset-password/:token" element={<ResetPassword />} />
-        <Route path="*" element={<Redirect to={`/`} />} />\
+        <Route path="*" element={<Redirect to={`/`} />} />
       </Routes>
     );
   }
@@ -54,24 +54,38 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<AppContainer />}>
-        <Route path="/owners" element={<Users />} />
-        <Route path="/owners/form" element={<UserForm />} />
-        <Route path="/owners/form/:id" element={<UserForm />} />
+        <Route path="owners" element={<Users />} />
+        <Route path="owners/form" element={<UserForm />} />
+        <Route path="owners/form/:id" element={<UserForm />} />
 
         <Route path="apartments" element={<Apartments />} />
         <Route path="apartments/:ownerId" element={<Apartments />} />
-        <Route path="/apartments/form" element={<ApartmentForm />} />
-        <Route path="/apartments/form/:roomName" element={<ApartmentForm />} />
+        <Route path="apartments/form" element={<ApartmentForm />} />
+        <Route path="apartments/form/:roomName" element={<ApartmentForm />} />
 
         <Route path="parkings" element={<Parkings />} />
         <Route path="parkings/:ownerId" element={<Parkings />} />
-        <Route path="/parkings/form" element={<ParkingForm />} />
-        <Route path="/parkings/form/:parkingName" element={<ParkingForm />} />
+        <Route path="parkings/form" element={<ParkingForm />} />
+        <Route path="parkings/form/:parkingName" element={<ParkingForm />} />
 
-        <Route index element={<Redirect to={user.Role === 'admin' ? '/owners' : `/owners/form/${user.OwnerID}`} />} />
+        <Route
+          index
+          element={
+            <Redirect
+              to={
+                user.Role === "admin"
+                  ? "/owners"
+                  : `/owners/form/${user.OwnerID}`
+              }
+            />
+          }
+        />
 
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Redirect to={`/not-found`} />} />
       </Route>
+
+      <Route path="not-found" element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
